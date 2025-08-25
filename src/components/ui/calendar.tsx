82bd542e057db435281e323e389bb76d4a01f646
xyzoptooo <dannydophlix@gmail.com>
2025-08-25 06:00:12 +0300

    cosmic v.0.2

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -52,8 +52,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
+        PreviousMonthButton: () => <ChevronLeft className="h-4 w-4" />,
+        NextMonthButton: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
