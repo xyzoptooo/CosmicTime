58522b7bef31a2bcab9014d309aa44cd49e834b3
xyzoptooo <dannydophlix@gmail.com>
2025-08-25 05:42:24 +0300

    cosmic v.0.2

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,25 +1,36 @@
 import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+
 import { cn } from "@/lib/utils"
 
-export interface BadgeProps extends React.HTMLProps<HTMLSpanElement> {
-  variant?: "default" | "secondary"
-}
-
-const Badge = React.forwardRef<HTMLSpanElement, BadgeProps>(
-  ({ className, variant = "default", ...props }, ref) => {
-    let variantClasses = ""
-    if (variant === "secondary") {
-      variantClasses = "bg-secondary text-secondary-foreground"
-    } else {
-      variantClasses = "bg-primary text-primary-foreground"
-    }
-    
-    return (
-      <span className={cn("inline-flex items-center px-2 py-1 text-sm font-medium rounded-full", variantClasses, className)} ref={ref} {...props} />
-    )
+const badgeVariants = cva(
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+        secondary:
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        destructive:
+          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        outline: "text-foreground",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
   }
 )
-Badge.displayName = "Badge"
 
-export { Badge }
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
 
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  )
+}
+
+export { Badge, badgeVariants }
