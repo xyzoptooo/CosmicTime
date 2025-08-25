0d1d4f0ed358951a29b4d833a4f3064f69be67ba
xyzoptooo <dannydophlix@gmail.com>
2025-08-25 07:41:17 +0300

    cosmic v.0.2

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import * as React from "react"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
 
-interface ProgressProps extends React.HTMLAttributes<HTMLDivElement> {
-  value: number;
-  max?: number;
-}
+import { cn } from "@/lib/utils"
 
-const Progress: React.FC<ProgressProps> = ({ value, max = 100, className = '', ...props }) => {
-  return (
-    <div className={`relative w-full h-2 bg-gray-200 rounded ${className}`} {...props}>
-      <div
-        className="absolute top-0 left-0 h-full bg-blue-500 rounded"
-        style={{ width: `${(value / max) * 100}%` }}
-      />
-    </div>
-  );
-};
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+>(({ className, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+      className
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="h-full w-full flex-1 bg-primary transition-all"
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+))
+Progress.displayName = ProgressPrimitive.Root.displayName
 
-export default Progress;
+export { Progress }
